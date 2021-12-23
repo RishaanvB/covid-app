e35861fec08986a720f0d4adced971890a44df30
Rishaan van Beveren <rishaan.vanbeveren@gmail.com>
2021-12-23 20:17:02 +0100

    add general layout

---
+++ components/Navbar.jsx
@@ -0,0 +1,13 @@
+export default function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <li>Europe</li>
+        <li>North America</li>
+        <li>South America</li>
+        <li>Asia</li>
+        <li>Africa</li>
+      </ul>
+    </nav>
+  );
+}
