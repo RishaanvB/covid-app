f323ae60e483231439563f51ea6c803b8868cbbd
Rishaan van Beveren <rishaan.vanbeveren@gmail.com>
2021-12-25 21:36:42 +0100

    add fetch

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,14 +1,13 @@
 import styles from '../styles/Navbar.module.css';
 
-const list = 'North-america, South-america, Africa, Asia, Europe'
-  .split(', ')
-  .map((item, index) => (
-    <li key={index} className={styles.listItem}>
-      {item}
-    </li>
-  ));
-
 export default function Navbar() {
+  const list = 'North-america, South-america, Africa, Asia, Europe'
+    .split(', ')
+    .map((item, index) => (
+      <li key={index} className={styles.listItem}>
+        {item}
+      </li>
+    ));
   return (
     <header className={styles.header}>
       <div className={styles.logo}>Covid</div>
--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,41 @@
 import dynamic from 'next/dynamic';
+import { useEffect, useState } from 'react';
 import Box from '../components/Box';
 import Layout from '../components/Layout';
 
 import styles from '../styles/Index.module.css';
 function HomePage() {
-  const MapComponent = dynamic(
+  const [data, setData] = useState({});
+  const [fake, setFake] = useState();
+  // USE NEXTJS SWR TO FETCH DATA: CHECK DOCS: https://swr.vercel.app/
+  useEffect(() => {
+    fetch('https://disease.sh/v3/covid-19/continents/europe?strict=true')
+      .then((response) => response.json())
+      .then((data) => setData(data));
+    console.log(data);
+  }, []);
+
+  /* const MapComponent = dynamic(
     () => import('../components/MapComponent'), // replace '@components/map' with your component's location
     { ssr: false } // This line is important. It's what prevents server-side render
-  );
+  ); */
+
+
   return (
     <Layout>
       <div className={styles.gridContainer}>
         <section className={styles.box1}>
           <Box>
             <p>Total Deaths: Europe</p>
-            <h1>25.000.000</h1>
-            <h1>1200 per million</h1>
+            <h1>
+              {data.deaths ? data.deaths?.toLocaleString() : 'loading...'}
+            </h1>
+
+            <h1>
+              {data.deathsPerOneMillion
+                ? data.deathsPerOneMillion?.toFixed(0).toLocaleString()
+                : 'Loading...'}
+            </h1>
           </Box>
         </section>
         <section className={styles.box2}>
@@ -33,7 +53,7 @@ function HomePage() {
         </section>
 
         <section className={styles.map}>
-          <MapComponent />
+          {/* <MapComponent /> */}
         </section>
         <section className={styles.graph1}>
           <Box>
